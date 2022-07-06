89726a6a76cad253f139f6ee797218bee9b8a70f
Ingrid-Nataly-Cruz-Espinosa <1719110501@utectulancingo.edu.mx>
2022-07-06 19:44:37 +0000

    ESTE ES EL MERO MERO

--- git/frontend/clientes.js
+++ git/frontend/clientes.js
@@ -2,34 +2,34 @@ function getClientes() {
 
     var request = new XMLHttpRequest();
     //Accede a la session de la pagina
-    usernombre = window.prompt('Usernombre:')
-    password = window.prompt('Password:')
+    username= sessionStorage.getItem("username");
+    password= sessionStorage.getItem("password");
+    
 
-    request.open('GET', "https://8000-ingridnatal-aplicacionw-uo942ndzvue.ws-us51.gitpod.io/clientes/");
+    request.open('GET', "https://8000-ingridnatal-aplicacionw-uo942ndzvue.ws-us51.gitpod.io/clientes/"); //pasamos el metodo y luego el endpoint que es la url
     request.setRequestHeader("Accept", "application/json");
-    request.setRequestHeader("Authorization", "Basic " + btoa(usernombre + ":" + password))
+    request.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password))
     request.setRequestHeader("content-type", "application/json");
     
-    const  tabla   = document.getElementById("tabla_clientes");
+    const  tabla   = document.getElementById("tabla_clientes");  //quiere decir que ya existe una tabla le decimos a la api que tome lo que se ya existe y que pueda eliminarlo y todo lo que necesitamos
 
-    var tblBody = document.createElement("tbody");
-    var tblHead = document.createElement("thead");
+    var tblBody = document.createElement("tbody");  //quiere decir que ya existe una tabla le decimos a la api que tome lo que se ya existe y que pueda eliminarlo y todo lo que necesitamos
+    var tblHead = document.createElement("thead"); //creamos una variable para el encabezado de la tabla
 
     tblHead.innerHTML = `
         <tr>
-            <th>ID Cliente</th>
-            <th>Nombre</th>
-            <th>Email</th>
+            <th>ID DEL CLIENTE</th>
+            <th>NOMBRE DEL CLIENTE</th>
+            <th>EMAIL</th>
         </tr>`;
 
     request.send();
 
    
-    request.onload = () => {
-        // Almacena la respuesta en una variable, si es 202 es que se obtuvo correctamente
+    request.onload = () => { 
         const response = request.responseText;
         const json = JSON.parse(response);
-        if (request.status === 401 || request.status === 403) {
+        if (request.status === 401 || request.status === 403) { //si me muestras un 301 o un 403 muestrame el siguiente mensaje
             alert(json.detail);
         }
         else if (request.status == 202){
